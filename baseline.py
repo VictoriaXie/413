945baa0a4f728b9ff7dae37eaed4854902c30caf
Wang Jessica Yan <wangje44@teach.cs.toronto.edu>
2024-04-18 20:42:38 -0400

    modified baseline models

--- baseline.py
+++ baseline.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import torch
 from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
@@ -7,50 +8,56 @@ from sklearn.model_selection import train_test_split
 import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
-from sklearn.model_selection import ParameterGrid
+import argparse
 
 class MyRNN(nn.Module):
     def __init__(self, model_w2v, hidden_size, num_classes):
         super(MyRNN, self).__init__()
-        self.vocab_size = len(model_w2v.wv)
+        self.vocab_size = len(model_w2v.wv)+1
         self.emb_size = model_w2v.vector_size
         self.hidden_size = hidden_size
         self.num_classes = num_classes
-        self.emb = nn.Embedding(self.vocab_size, self.emb_size)
+        self.emb = nn.Embedding(self.vocab_size, self.emb_size, padding_idx=0)
+        # Add an extra zero-initialized vector as the padding vector
+        self.emb.weight.data.copy_(torch.from_numpy(np.vstack((np.zeros((1, self.emb_size)), model_w2v.wv.vectors))))
+        self.emb = nn.Embedding.from_pretrained(torch.from_numpy(model_w2v.wv.vectors), freeze=True)
         self.rnn = nn.RNN(self.emb_size, hidden_size, bidirectional=True, batch_first=True)
+        self.dropout = nn.Dropout(p=0.2)
         self.fc = nn.Linear(hidden_size * 2, num_classes)
 
     def forward(self, X):
         embedded = self.emb(X)
         _, hidden = self.rnn(embedded)
         hidden = torch.cat((hidden[-2,:,:], hidden[-1,:,:]), dim=1)
-        output = self.fc(hidden)
+        dropped = self.dropout(hidden)
+        output = self.fc(dropped)
         return output
 
     def parameters(self):
         for name, param in self.named_parameters():
             if name != 'emb.weight':
                 yield param
+
 
 class MyLSTM(nn.Module):
     def __init__(self, model_w2v, hidden_size, num_classes):
         super(MyLSTM, self).__init__()
-        self.vocab_size = len(model_w2v.wv)
+        self.vocab_size = len(model_w2v.wv) + 1
         self.emb_size = model_w2v.vector_size
         self.hidden_size = hidden_size
         self.num_classes = num_classes
-        self.emb = nn.Embedding(self.vocab_size, self.emb_size)
+        self.emb = nn.Embedding(self.vocab_size, self.emb_size, padding_idx=0)
+        # Add an extra zero-initialized vector as the padding vector
+        self.emb.weight.data.copy_(torch.from_numpy(np.vstack((np.zeros((1, self.emb_size)), model_w2v.wv.vectors))))
         self.lstm = nn.LSTM(self.emb_size, hidden_size, num_layers=2, bidirectional=True, batch_first=True)
-        self.fc = nn.Linear(hidden_size, num_classes)
-        self.softmax = nn.LogSoftmax()
         self.dropout_layer = nn.Dropout(p=0.2)
+        self.fc = nn.Linear(hidden_size * 2, num_classes)
 
     def forward(self, X):
         embedded = self.emb(X)
         outputs, (ht, ct) = self.lstm(embedded)
-        output = self.dropout_layer(ht[-1])
+        output = self.dropout_layer(torch.cat((ht[-2], ht[-1]), dim=1))
         output = self.fc(output)
-        output = self.softmax(output)
         return output
 
     def parameters(self):
@@ -58,7 +65,8 @@ class MyLSTM(nn.Module):
             if name != 'emb.weight':
                 yield param
 
-def accuracy(model, dataset, max=1000):
+
+def accuracy(model, dataset):
     """
     Estimate the accuracy of `model` over the `dataset`.
     We will take the **most probable class**
@@ -67,32 +75,30 @@ def accuracy(model, dataset, max=1000):
     Parameters:
         `model`   - An object of class nn.Module
         `dataset` - A dataset of the same type as `train_data`.
-        `max`     - The max number of samples to use to estimate
-                    model accuracy
 
     Returns: a floating-point value between 0 and 1.
     """
-
+    model.eval()
     correct, total = 0, 0
     dataloader = DataLoader(dataset,
-                            batch_size=1,  # use batch size 1 to prevent padding
+                            batch_size=20, 
                             collate_fn=collate_batch)
     for i, (x, t) in enumerate(dataloader):
+        x, t = x.to(device), t.to(device)
         z = model(x)
         y = torch.argmax(z, axis=1)
         correct += int(torch.sum(t == y))
-        total   += 1
-        if i >= max:
-            break
+        total += t.size(0)
+
+    model.train()
     return correct / total
 
 def train_model(model,                # an instance of MLPModel
                 train_data,           # training data
                 val_data,             # validation data
-                learning_rate=0.001,
-                batch_size=100,
+                learning_rate=0.0001,
+                batch_size=314,
                 num_epochs=10,
-                plot_every=1000,        # how often (in # iterations) to track metrics
                 plot=True):           # whether to plot the training curve
     train_loader = torch.utils.data.DataLoader(train_data,
                                                batch_size=batch_size,
@@ -103,15 +109,14 @@ def train_model(model,                # an instance of MLPModel
 
     # these lists will be used to track the training progress
     # and to plot the training curve
-    iters, train_loss, train_acc, val_acc = [], [], [], []
-    iter_count = 0 # count the number of iterations that has passed
-    best_val_acc = 0
+    train_loss, train_acc, val_acc = [], [], []
 
     try:
         for e in range(num_epochs):
             model.train()
             for i, (texts, labels) in enumerate(train_loader):
-                texts = torch.tensor(texts).long()
+                texts = texts.to(device)
+                labels = labels.to(device)
                 z = model(texts) 
                 loss = criterion(z, labels) 
 
@@ -119,36 +124,34 @@ def train_model(model,                # an instance of MLPModel
                 optimizer.step() # update the parameters
                 optimizer.zero_grad() # clean up accumualted gradients
 
-                iter_count += 1
-                if iter_count % plot_every == 0:
-                    iters.append(iter_count)
-                    ta = accuracy(model, train_data)
-                    va = accuracy(model, val_data)
-                    train_loss.append(float(loss))
-                    train_acc.append(ta)
-                    val_acc.append(va)
-                    if va > best_val_acc:
-                        best_val_acc = va
-                        torch.save(model.state_dict(), "best_model.pth")
-                    print(iter_count, "Loss:", float(loss), "Train Acc:", ta, "Val Acc:", va)
+            ta = accuracy(model, train_data)
+            va = accuracy(model, val_data)
+            train_loss.append(float(loss))
+            train_acc.append(ta)
+            val_acc.append(va)
+            print(e, "Loss:", float(loss), "Train Acc:", ta, "Val Acc:", va)
+
     finally:
         # This try/finally block is to display the training curve
         # even if training is interrupted
         if plot:
             plt.figure()
-            plt.plot(iters[:len(train_loss)], train_loss)
+            plt.plot(range(num_epochs), train_loss)
             plt.title("Loss over iterations")
             plt.xlabel("Iterations")
             plt.ylabel("Loss")
+            plt.savefig("loss.png")
+            plt.close()
 
             plt.figure()
-            plt.plot(iters[:len(train_acc)], train_acc)
-            plt.plot(iters[:len(val_acc)], val_acc)
+            plt.plot(range(num_epochs), train_acc)
+            plt.plot(range(num_epochs), val_acc)
             plt.title("Accuracy over iterations")
             plt.xlabel("Iterations")
-            plt.ylabel("Loss")
+            plt.ylabel("Accuracy")
             plt.legend(["Train", "Validation"])
-            plt.savefig("test.png")
+            plt.savefig("acc.png")
+            plt.close()
 
 
 def convert_indices(data, model_w2v):
@@ -157,12 +160,10 @@ def convert_indices(data, model_w2v):
         indices = []
         for w in words:
             if w in model_w2v.wv.key_to_index:
-                indices.append(model_w2v.wv.key_to_index.get(w))
+                # Shift indices up by one since the padding token is at index 0
+                indices.append(model_w2v.wv.key_to_index.get(w) + 1) 
             else:
-                # this is a bit of a hack, but we will repurpose *last* word
-                # (least common word) appearing in the w2v vocabluary as our
-                # '<pad>' token
-                indices.append(len(model_w2v.wv)-1)
+                indices.append(0)
         result.append((indices, label),)
     return result
 
@@ -187,28 +188,28 @@ def collate_batch(batch):
         text_list.append(torch.tensor(text_indices))
         label_list.append(label)
 
-    # TODO CONFIRM THIS PADDING VALUE!!!!   
-    X = pad_sequence(text_list, padding_value=len(model_w2v.wv)-1).transpose(0, 1).to(device)
-    t = torch.tensor(label_list, dtype=torch.long).to(device)
+    X = pad_sequence(text_list, padding_value=0, batch_first=True)
+    t = torch.tensor(label_list, dtype=torch.long)
     return X, t
 
-if __name__ == "__main__":
-    device = torch.device('cuda:0')
 
-    # human_token = pd.read_csv("human_token.csv", index_col=0)
-    # ai_token = pd.read_csv("ai_token.csv", index_col=0)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', type=str, default='RNN') # Choose RNN or LSTM
+    args = parser.parse_args()  
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    torch.manual_seed(22)
+
+    human_token = pd.read_csv("human_token.csv", index_col=0)
+    ai_token = pd.read_csv("ai_token.csv", index_col=0)
     model_w2v = Word2Vec.load("w2vmodel.model")
-    # all_tokens = pd.read_csv("all.csv", index_col=0)
 
     # Combine human token and ai token
-    # token = pd.concat([human_token, ai_token], ignore_index=True)
-
-    data = pd.read_csv('all.csv')
-    text = data.iloc[:, 0].to_numpy()
-    flag = data.iloc[:, 1].to_numpy()
+    token = pd.concat([human_token, ai_token], ignore_index=True)
 
     # Shuffle and split the data
-    X_train, X_temp, y_train, y_temp = train_test_split(text, flag, test_size=0.2, random_state=42)
+    X_train, X_temp, y_train, y_temp = train_test_split(token["text"], token["human_wrote"], test_size=0.3, random_state=42)
     X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=42)
     train_data = list(zip(X_train, y_train))
     val_data = list(zip(X_val, y_val))
@@ -219,33 +220,15 @@ if __name__ == "__main__":
     val_data_indices = convert_indices(val_data, model_w2v)
     test_data_indices = convert_indices(test_data, model_w2v)
 
+    if args.model == "RNN":
+        model = MyRNN(model_w2v, hidden_size=300, num_classes=2).to(device)
+        train_model(model, train_data_indices, val_data_indices, batch_size=400)
+    elif args.model == "LSTM":
+        model = MyLSTM(model_w2v, hidden_size=256, num_classes=2).to(device)
+        train_model(model, train_data_indices, val_data_indices, batch_size=314)
+    
+    print(f"test accuracy: {accuracy(model, test_data_indices)}")
+    torch.save(model.state_dict(), f"models/best_{args.model}.pth")
+        
 
-    # model = MyRNN(model_w2v,
-    #           hidden_size=64,
-    #           num_classes=2)
-    # model = model.to(device)
-
-    # train_model(model, train_data_indices, val_data_indices, batch_size=32, num_epochs=100)
-
-    model = MyLSTM(model_w2v,
-              hidden_size=64,
-              num_classes=2)
-    model.load_state_dict(torch.load("best_lstm.pth"))
-    model = model.to(device)
-    model.eval()
-    acc = accuracy(model, test_data_indices)
-    print(acc)
-
-
-
-#     param_grid = {
-#     'batch_size': [32, 64, 128],
-#     'learning_rate': [0.001, 0.0001],
-#     'num_epochs': [5, 10]
-# }
-#     grid = list(ParameterGrid(param_grid))
-#     for params in grid:
-#     train_model(model, train_data, val_data, 
-#                 learning_rate=params['learning_rate'], 
-#                 batch_size=params['batch_size'], 
-#                 num_epochs=params['num_epochs'])
+    
